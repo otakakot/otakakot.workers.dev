beb38d1a680d59170c87c162a44a807e06fa7fa0
otakakot <117251387+otakakot@users.noreply.github.com>
2026-08-16 09:04:23 +0900

    first commit

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/syumai/workers"
+	_ "github.com/syumai/workers/cloudflare/d1"
 )
 
 func main() {
 	http.HandleFunc("GET /", func(w http.ResponseWriter, req *http.Request) {
-		msg := "Hello!"
-		w.Write([]byte(msg))
+		db, err := sql.Open("d1", "DB")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusServiceUnavailable)
+			return
+		}
+		defer db.Close()
+
+		var one int
+		if err := db.QueryRowContext(req.Context(), "SELECT 1").Scan(&one); err != nil {
+			http.Error(w, err.Error(), http.StatusServiceUnavailable)
+			return
+		}
+
+		w.Write([]byte("health"))
 	})
 	workers.Serve(nil) // use http.DefaultServeMux
 }
